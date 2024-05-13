911b9292c6d0651bf2fe41ef09f480f533a17a26
dubox21 <lorenadubox@gmail.com>
2024-05-13 13:59:59 -0300

    se agrega accion al input de busqueda

--- scripts/library.js
+++ scripts/library.js
@@ -25,3 +25,60 @@ document.getElementById('btn-liked').addEventListener('click', function () {
         behavior: "smooth" // Desplazamiento suave
     });
 });
+
+const searchInput = document.getElementById('searchInput');
+const searchResultsContainer = document.getElementById('searchResults');
+const noResultsMessage = document.getElementById('noResultsMessage');
+
+const handleSearch = async () => {
+    const searchValue = searchInput.value.toLowerCase();
+
+    try {
+        // Cargar los datos de las películas
+        const response = await fetch('../JSON/movies.json');
+        const moviesData = await response.json();
+
+        const filteredMovies = moviesData.filter(movie => movie.title.toLowerCase().startsWith(searchValue));
+
+        // Limpiar resultados anteriores
+        searchResultsContainer.innerHTML = '';
+
+        // Mostrar sugerencias de búsqueda
+        if (filteredMovies.length > 0 && searchValue !== '') {
+            // Mostrar sugerencias de búsqueda
+            filteredMovies.forEach(movie => {
+                const resultItem = document.createElement('a');
+                resultItem.textContent = movie.title;
+                resultItem.classList.add('search-result');
+
+                // Al hacer clic en la sugerencia, ir a la página de detalles de esa película
+                resultItem.addEventListener('click', () => {
+                    window.location.href = `../Templates/movie.html?title=${encodeURIComponent(movie.title)}`;
+                });
+
+                searchResultsContainer.appendChild(resultItem);
+            });
+           // Mostrar el contenedor de resultados
+           searchResultsContainer.style.display = 'block';
+
+           // Ocultar el mensaje de no resultados
+           if (noResultsMessage) {
+               noResultsMessage.style.display = 'none';
+           }
+       } else {
+           // Si no hay coincidencias, ocultar el contenedor de resultados
+           searchResultsContainer.style.display = 'none';
+           // Mostrar el mensaje de no resultados solo si el campo de búsqueda no está vacío
+           if (searchValue.trim() !== '') {
+               noResultsMessage.style.display = 'block';
+           } else {
+               noResultsMessage.style.display = 'none'; // Ocultar el mensaje si el campo está vacío
+           }
+       }
+    } catch (error) {
+        console.error('Error al cargar los datos de las películas:', error);
+    }
+};
+
+// Asignar el evento de búsqueda al campo de entrada
+searchInput.addEventListener('input', handleSearch);
