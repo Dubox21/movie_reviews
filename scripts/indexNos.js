0fb92954351af8975a84870a30b83e85df36baf3
Ximena <ximenamartin33@gmail.com>
2024-05-23 19:25:01 +0100

    modifico archivo indexNos.js para dar funcion a btn de Acerca-de-nosotros

--- scripts/indexNos.js
+++ scripts/indexNos.js
@@ -10,3 +10,16 @@ navToggle.addEventListener("click", () => {
         navToggle.setAttribute("aria-label", "Cerrar menu");
     }
 });
+
+
+//Funcion para posicionarse en las tarjetas de Acerca de nosotros//
+document.getElementById('btn-mas').addEventListener('click', function () {
+    const headerHeight = document.querySelector('.header').offsetHeight; // Obtener la altura del encabezado
+    const SeccionCards = document.getElementById('seccioncard').offsetTop - headerHeight; // Ajustar la posición
+
+    // Desplazarse a la posición de la seccion de las tarjetas //
+    window.scrollTo({
+        top: SeccionCards,
+        behavior: "smooth" // Desplazamiento suave
+    });
+});
