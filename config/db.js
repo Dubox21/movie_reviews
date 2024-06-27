7d3b6d7191da9a94020223fa7ae4266279f47737
dubox21 <lorenadubox@gmail.com>
2024-06-27 12:19:22 -0300

    se sube base de datos

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2';
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '0000',
-    database: 'movies_reviews'
+    host: process.env.MYSQL_ADDON_HOST,
+    user: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB
 });
 
 db.connect(err => {
